4ad2da708b93a0099dd4bddd6dc47f2a4fd62834
4ernish <arcad@DESKTOP-TMFLOFK>
2024-12-05 02:19:36 +0300

    Добавлены модели (опять)

---
+++ API/Models/Lesson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Models;
+
+public partial class Lesson
+{
+    public int GroupId { get; set; }
+
+    public int SubjectId { get; set; }
+
+    public DateOnly Date { get; set; }
+
+    public int Number { get; set; }
+
+    public virtual Group Group { get; set; } = null!;
+
+    public virtual Subject Subject { get; set; } = null!;
+}
---
+++ API/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Models;
+
+public partial class User
+{
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Fio { get; set; } = null!;
+
+    public DateOnly Birthday { get; set; }
+
+    public int? GroupId { get; set; }
+
+    public int RoleId { get; set; }
+
+    public virtual Group? Group { get; set; }
+
+    public virtual Role Role { get; set; } = null!;
+
+    public virtual ICollection<Subject> Subjects { get; set; } = new List<Subject>();
+}
