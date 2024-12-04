ac20140d59586bd294bb5591a7c7dffe679a019f
VolnovF <diegogonsales2019@gmail.com>
2024-12-04 16:01:26 +0300

    DbContext

---
+++ API/BaseDbContext.cs
@@ -0,0 +1,25 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class BaseDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Stud> Students { get; set; }
+        public DbSet<Prep> Teachers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Speciality> Specialities { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+
+        public DbSet<Ekz> Ekz { get; set; }
+        public DbSet<Mera> Mera { get; set; }
+        public DbSet<Timetable> Timetable { get; set; }
+
+
+    }
+}
