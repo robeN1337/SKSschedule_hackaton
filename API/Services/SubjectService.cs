d65fe86cd3c76a7c694b9f2080522e3e6b1c7d9e
showlee <rashergogle@gmail.com>
2024-12-05 04:10:33 +0300

    subjectservice

---
+++ API/Services/SubjectService.cs
@@ -0,0 +1,46 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services
+{
+    public class SubjectService
+    {
+        private readonly DbSksContext _db = new DbSksContext();
+
+        public async Task createSubject(Subject subject)
+        {
+            await _db.Subjects.AddAsync(subject);
+            await _db.SaveChangesAsync();
+        }
+        public async Task<List<Subject>> getAllSubjects()
+        {
+            return await _db.Subjects
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Subject?> getSubject(string name)
+        {
+            return _db.Subjects
+                .AsNoTracking()
+                .FirstOrDefault(s => s.Name == name);
+        }
+
+        public async Task updateSubject(Subject subject)
+        {
+            await _db.Subjects
+                .Where(s => s.Name == subject.Name)
+                .ExecuteUpdateAsync(s => s
+                    .SetProperty(s => s.Description, subject.Description)
+                    .SetProperty(s => s.Cabinet, subject.Cabinet)
+                    .SetProperty(s => s.UserLogin, subject.UserLogin));
+        }
+
+        public async Task deleteSubject(Subject subject)
+        {
+            await _db.Subjects
+                .Where(s => s.Name == subject.Name)
+                .ExecuteDeleteAsync();
+        }
+    }
+}
